b9edbebe5d44886abc6555251502a26c719cd333
Nick Wong <njwon4@gmail.com>
2016-08-31 12:55:31 +1000

    End of week 6 lab

--- TaskList/scripts/tasks-controller.js
+++ TaskList/scripts/tasks-controller.js
@@ -7,6 +7,12 @@ tasksController = function() {
         console.log(errorCode + ':' + errorMessage);
     }
 
+    /* Recolor the Table Rows */
+    var recolor = function() {
+        $('#tblTasks tbody').find('tr').removeClass('even');
+        $('#tblTasks tbody').find('tr:even').addClass('even');
+    };
+
     return {
         // Public data and methods
         init: function(page, callback) {
@@ -39,12 +45,6 @@ tasksController = function() {
                 /* Show the 'required' fields in the form */
                 $taskFormRequired.prev('label').append('<span>*</span>').children('span').addClass('required');
 
-                /* Recolor the Table Rows */
-                var recolor = function() {
-                    $tableBody.find('tr').removeClass('even');
-                    $tableBody.find('tr:even').addClass('even');
-                };
-
                 /* Show New Task Form */
                 $btnAddTask.click(function(evt) {
                     evt.preventDefault();
@@ -64,6 +64,42 @@ tasksController = function() {
                             $(evt.target).parents('tr').remove();
                             recolor();
                         }, errorLogger);
+                });
+
+                /* Delete Selected Tasks */
+                $btnDeleteSelection.click(function(evt) {
+                    evt.preventDefault();
+
+                    // Get the highlighted tr elements
+                    var $highlighted = $tableBody.find('tr td:first-child[class="rowHighlight"]');
+                    var num_selected_tasks = $highlighted.length;
+
+                    if (num_selected_tasks === 0) {
+                        alert("No tasks selected!");
+                    } else if (confirm("Delete " + num_selected_tasks + " selected tasks!\nAre you sure?")) {
+
+                        // For each of the highlighted rows
+                        $highlighted.each(function() {
+
+                            var $this_row = $(this);
+
+                            // Delete from the storage engine
+                            storageEngine.delete('task', parseInt($this_row.data().taskId),
+                            function() {
+                                // Update the UI on success
+                                $this_row.closest('tr').remove();
+                                recolor();
+                            }, errorLogger);
+                        });
+
+                        // Need to put the alert in a setTimeout since alert boxes are blocking
+                        setTimeout(function() {
+                            alert(num_selected_tasks + " tasks deleted!");
+                        }, 100);
+
+                    } else {
+                        alert("Delete cancelled");
+                    }
                 });
 
                 /* Set up the validation rules for the form */
@@ -95,33 +131,7 @@ tasksController = function() {
                                 $(':input').val('');
                                 $taskPage.find('#taskCreation').addClass('not');
                                 // $taskRowTmpl.tmpl(task).appendTo($tableBody);
-                                recolor();
                             }, errorLogger);
-                    }
-                });
-
-                /* Delete Selected Tasks */
-                $btnDeleteSelection.click(function(evt) {
-                    evt.preventDefault();
-
-                    var $highlighted = $tableBody.find('tr td:first-child[class="rowHighlight"]');
-                    var num_selected_tasks = $highlighted.length;
-
-                    if (num_selected_tasks === 0) {
-                        alert("No tasks selected!");
-                    } else if (confirm("Delete " + num_selected_tasks + " selected tasks!\nAre you sure?")) {
-                        $highlighted.each(function() {
-                            $(this).closest('tr').remove();
-                            recolor();
-                        });
-
-                        // Need to put the alert in a setTimeout since alert boxes are blocking
-                        setTimeout(function() {
-                            alert(num_selected_tasks + " tasks deleted!");
-                        }, 1);
-
-                    } else {
-                        alert("Delete cancelled");
                     }
                 });
 
@@ -141,6 +151,7 @@ tasksController = function() {
             storageEngine.findAll('task', function(tasks) {
                 $.each(tasks,function(index, task) {
                     $('#taskRow').tmpl(task).appendTo('#tblTasks tbody');
+                    recolor();
                 });
             }, errorLogger);
         }
